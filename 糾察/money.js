2b513aafe8cb22c0fa5ecb1c32e3eb36751521c7
lys1004 <79405017+5487556@users.noreply.github.com>
2024-08-22 13:32:14 +0800

    Update money.js

--- 糾察/money.js
+++ 糾察/money.js
@@ -6,40 +6,23 @@ function updateCashBalance() {
 }
 
 function loadTransactions() {
-    fetch('http://localhost:3000/api/transactions')
-        .then(response => response.json())
-        .then(transactions => {
-            transactions.forEach(transaction => {
+    const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+        transactions.forEach(transaction => {
                 const newRow = `<tr>
-                <td>${transaction.date}</td>
-                <td>${transaction.description}</td>
-                <td>${transaction.amount}</td>
-                <td>${transaction.type === 'income' ? '收入' : '支出'}</td>
-                <td><a href="#">編輯</a> | <a href="#">刪除</a></td>
-            </tr>`;
+                    <td>${transaction.date}</td>
+                    <td>${transaction.description}</td>
+                    <td>${transaction.amount}</td>
+                    <td>${transaction.type === 'income' ? '收入' : '支出'}</td>
+                    <td><a href="#">編輯</a> | <a href="#">刪除</a></td>
+                </tr>`;
                 document.querySelector('table tbody').insertAdjacentHTML('beforeend', newRow);
             });
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
 }
 
 function saveTransaction(transaction) {
-    fetch('http://localhost:3000/api/transactions', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(transaction)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Transaction saved:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
+    transactions.push(transaction);
+    localStorage.setItem('transactions', JSON.stringify(transactions));
 }
 
 document.getElementById('transaction-form').addEventListener('submit', function (event) {
